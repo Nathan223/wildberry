3ba03d0b1f83e63a25d64bffc2fbbaca133c4ea3
maroj44 <31449735+maroj44@users.noreply.github.com>
2018-11-10 16:07:27 -0600

    First

---
+++ Cherry.py
@@ -0,0 +1,18 @@
+from Fruit import Fruit
+
+
+class Cherry(Fruit):
+    def __init__(self):
+        Fruit.__init__(self)
+
+    def quickAttack(self):
+        pass
+
+    def secondaryAttack(self):
+        pass
+
+    def utility(self):
+        pass
+
+    def ultimate(self):
+        pass
---
+++ Fruit.py
@@ -0,0 +1,17 @@
+class Fruit:
+    def __init__(self):
+        self.HP
+        self.xPos
+        self.yPos
+
+    def quickAttack(self):
+        pass
+
+    def secondaryAttack(self):
+        pass
+
+    def utility(self):
+        pass
+
+    def ultimate(self):
+        pass
