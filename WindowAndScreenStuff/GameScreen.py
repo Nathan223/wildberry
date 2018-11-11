ccd1cbe28c97301fa17cadfd728f5d0062dd4810
Francis Rinaldi <francis.rinaldi@gmail.com>
2018-11-11 01:41:42 -0600

    Did stuff

--- WindowAndScreenStuff/GameScreen.py
+++ WindowAndScreenStuff/GameScreen.py
@@ -1,26 +1,28 @@
 from Stage import Stage
 from graphics import Text,Image,Point
-from hitbox import hitbox
+from physics2 import hitbox
 import time
+
 
 class GameScreen:
     def __init__(self, window, player1, player2):
         self.p1 = player1
         self.p2 = player2
-        self.hb1 = None
-        self.hb2 = None
-        self.p1Pic = None
-        self.p2Pic = None
         self.stage = Stage(window)
-        #Stage.createStage()
         Stage.regStage(self.stage, window)
+        self.p1Point = Point(self.stage.getPos1().getX(), self.stage.getPos1().getY() - (89 + 6))
+        self.p2Point = Point(self.stage.getPos2().getX(), self.stage.getPos2().getY() - (89 + 6))
+        self.p1Pic = Image(self.p1Point,self.p1.getImage())
+        self.p2Pic = Image(self.p2Point,self.p2.getImage())
+        #Stage.createStage()
         self.createHealthBars(window)
         self.startGame(window)
-        self.countDown = Text(Point(window.getWidth() / 2, window.getHeight() / 2), Text)
+        self.countDown = Text(Point(window.getWidth() / 2, window.getHeight() / 2), '')
         self.countdown(window)
         while True:
             if self.checkExit(window.checkKey()):
                 window.close()
+            self.update()
 
     def checkExit(self, key):
         if key == 'Escape':
@@ -49,9 +51,9 @@ class GameScreen:
         for i in range(3, 0, -1):
             self.countDown.setText(str(i))
             if i==2:
-                self.loadP1(window)
+                self.p1Pic.draw(window)
             if i==1:
-                self.loadP2(window)
+                self.p2Pic.draw(window)
             time.sleep(1)
         self.countDown.setText("Fight")
         self.countDown.setSize(36)
@@ -59,13 +61,7 @@ class GameScreen:
         self.countDown.setText("")
 
     def loadP1(self, window):
-        p1Point = Point(self.stage.getPos1().getX(),self.stage.getPos1().getY()-(89+6))
-        self.p1Pic = Image(p1Point,self.p1.getImage())
-        #self.hb1 = hitbox()
         self.p1Pic.draw(window)
 
     def loadP2(self, window):
-        p2Point = Point(self.stage.getPos2().getX(), self.stage.getPos2().getY() - (89 + 6))
-        self.p2Pic = Image(p2Point, self.p2.getImage())
-        #self.hb2 = hitbox()
         self.p2Pic.draw(window)
