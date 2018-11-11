15c0a6b05d0a67e852f177564e034bcf46b9fa16
maroj44 <31449735+maroj44@users.noreply.github.com>
2018-11-10 23:02:25 -0600

    Add files via upload

---
+++ Apple.py
@@ -0,0 +1,22 @@
+from Fruit import Fruit
+
+
+class Apple(Fruit):
+    def __init__(self):
+        Fruit.__init__(self)
+        self.image = "ImagesAndSprites/Apple.gif"
+
+    def quickAttack(self):
+        pass
+
+    def secondaryAttack(self):
+        pass
+
+    def utility(self):
+        pass
+
+    def ultimate(self):
+        pass
+
+    def getImage(self):
+        return self.image
