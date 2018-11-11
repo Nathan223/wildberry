3210104c3e1f7e7990892f5e5827843bb5e35ddf
Francis Rinaldi <francis.rinaldi@gmail.com>
2018-11-10 21:09:09 -0600

    A

--- Stage.py
+++ Stage.py
@@ -1,7 +1,9 @@
 from updatedGraphics import *
 class Stage(object):
     def __init__(self,window):
-        self.pos = 1
+        def __init__(self, window):
+            self.pos1 = Point(0, 0)
+            self.pos2 = Point(0, 0)
 
     def regStage(self,window):
         background = Image(Point(600,400),"ImagesAndSprites/StartScreen.gif")
@@ -9,3 +11,4 @@ class Stage(object):
         stage = Rectangle(Point(200,650),Point(1000,700))
         stage.setFill("Blue")
         stage.draw(window)
+        self.pos1 = Point
