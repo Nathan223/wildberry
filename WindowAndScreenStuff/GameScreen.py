8d6ed7f50a0274418ff70ce1f7389db63b1150e3
Manraj Kumar <manrajmanshi123@gmail.com>
2018-11-10 21:55:14 -0600

    countdown

--- WindowAndScreenStuff/GameScreen.py
+++ WindowAndScreenStuff/GameScreen.py
@@ -2,21 +2,33 @@ from Stage import Stage
 from updatedGraphics import *
 
 class GameScreen:
-    def __init__(self,window):
+    def __init__(self, window):
         stage = Stage(window)
         #Stage.createStage()
-        Stage.regStage(stage,window)
+        Stage.regStage(stage, window)
         self.createHealthBars(window)
         self.startGame(window)
-        self.countDown(window)
-        while(True):
+        self.countDown = Text(Point(window.getWidth() / 2, window.getHeight() / 2), Text)
+        self.countdown(window)
+        while True:
             pass
 
-    def createHealthBars(self,window):
+    def createHealthBars(self, window):
         pass
 
-    def startGame(self,window):
+    def startGame(self, window):
         pass
 
-    def countDown(self,window):
-        pass
+    def countdown(self, window):
+
+        self.countDown.setTextColor("White")
+        self.countDown.setSize(20)
+        self.countDown.draw(window)
+        self.countDown.setSize(36)
+        for i in range(3, 0, -1):
+            self.countDown.setText(str(i))
+            time.sleep(1)
+        self.countDown.setText("Fight")
+        self.countDown.setSize(36)
+        time.sleep(1)
+        self.countDown.setText("")
