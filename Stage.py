15a7e4bdfe2a4273a8f2e2ea55d6d34feeae3fba
Manraj Kumar <manrajmanshi123@gmail.com>
2018-11-10 20:46:29 -0600

    stage

--- Stage.py
+++ Stage.py
@@ -6,6 +6,6 @@ class Stage(object):
     def regStage(self,window):
         background = Image(Point(600,400),"ImagesAndSprites/StartScreen.gif")
         background.draw(window)
-        stage = Rectangle(Point(200,500),Point(1000,700))
+        stage = Rectangle(Point(200,650),Point(1000,700))
         stage.setFill("Blue")
         stage.draw(window)
