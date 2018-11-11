2a401af3ea8e383b9d5511c2726f66d00b9bc2f8
Manraj Kumar <manrajmanshi123@gmail.com>
2018-11-10 20:28:19 -0600

    Update Stage.py

--- Stage.py
+++ Stage.py
@@ -1,12 +1,12 @@
-import random as rm
-# rm.randint(1,10)
-class Stage:
-
-    def __init__(self,window):
-
-
-    def plainStage(self):
-
-    def createStage(self):
-        pass
-
+from updatedGraphics import *
+class Stage(object):
+    def __init__(self,win:GraphWin):
+        self.background = Rectangle(Point(0,0), Point(1200,800))
+        self.background.setFill("Black")
+        self.background.draw(win)
+        for i in range(3,0,-1):
+            self.background
+    def regStage(self,win:GraphWin):
+        self.stage = Rectangle(Point(200,500),Point(1000,700))
+        self.stage.setFill("Blue")
+        self.stage.draw(win)
