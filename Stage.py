20fb826f844d5e250fe23e60b690054891fb10be
Francis Rinaldi <francis.rinaldi@gmail.com>
2018-11-10 21:23:10 -0600

    A

--- Stage.py
+++ Stage.py
@@ -1,9 +1,10 @@
-from updatedGraphics import *
+from updatedGraphics import Image,Point,Rectangle
+
+
 class Stage(object):
-    def __init__(self,window):
-        def __init__(self, window):
-            self.pos1 = Point(0, 0)
-            self.pos2 = Point(0, 0)
+    def __init__(self, window):
+        self.pos1 = Point(0, 0)
+        self.pos2 = Point(0, 0)
 
     def regStage(self,window):
         background = Image(Point(600,400),"ImagesAndSprites/StartScreen.gif")
@@ -11,4 +12,11 @@ class Stage(object):
         stage = Rectangle(Point(200,650),Point(1000,700))
         stage.setFill("Blue")
         stage.draw(window)
-        self.pos1 = Point
+        self.pos1 = Point(400,700)
+        self.pos2 = Point(800,700)
+
+    def getPos1(self):
+        return self.pos1
+
+    def getPos2(self):
+        return self.pos2
