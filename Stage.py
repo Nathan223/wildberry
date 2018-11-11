ced61ee68197efa76d527b7c70818318ba41b151
Francis Rinaldi <francis.rinaldi@gmail.com>
2018-11-10 20:03:04 -0600

    Did Stuff

--- Stage.py
+++ Stage.py
@@ -2,6 +2,9 @@ import random as rm
 # rm.randint(1,10)
 class Stage:
 
+    def __init__(self,window):
+        
+
     def plainStage(self):
 
     def getStages(self):
--- WindowAndScreenStuff/GameScreen.py
+++ WindowAndScreenStuff/GameScreen.py
@@ -1,8 +1,14 @@
 from Stages import getStages
-from random import
+from random import randint
 
 
 class GameScreen:
     def __init__(self,window):
+        #ran = randint(0,getStages().length)
+        getStages(window)
+        self.createHealthBars(window)
+
+    def createHealthBars(self,window):
+        pass
 
 
