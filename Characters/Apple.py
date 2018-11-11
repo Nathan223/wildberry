36a9d1b9d46696c93d48970b9e5831f87449b5e2
Francis Rinaldi <francis.rinaldi@gmail.com>
2018-11-10 23:26:03 -0600

    Fix

--- Characters/Apple.py
+++ Characters/Apple.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from Fruit import Fruit
 
 
@@ -20,27 +19,4 @@ class Apple(Fruit):
         pass
 
     def getImage(self):
-=======
-from Fruit import Fruit
-
-
-class Apple(Fruit):
-    def __init__(self):
-        Fruit.__init__(self)
-        self.image = "ImagesAndSprites/Apple.gif"
-
-    def quickAttack(self):
-        pass
-
-    def secondaryAttack(self):
-        pass
-
-    def utility(self):
-        pass
-
-    def ultimate(self):
-        pass
-
-    def getImage(self):
->>>>>>> db4992b12f3c154c67fc554fef579dab57fa28e2
-        return self.image
+        return self.imagea
