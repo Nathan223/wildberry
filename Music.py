3494b2083861d5f28dc075ba8b7ce65b6440e36f
Gabi Gergely <gabi@purchasingplatform.com>
2018-11-13 12:48:36 -0600

    Fix audio file loading based on operating system

--- Music.py
+++ Music.py
@@ -1,5 +1,22 @@
-try:
+import platform
+
+def load_windows():
     import winsound
     winsound.PlaySound("Audio/wildberry.wav", winsound.SND_FILENAME)
-except ImportError:
+
+def load_mac():
+    import subprocess
+    audio_file = "./Audio/wildberry.wav"
+    subprocess.call(["afplay", audio_file])
+
+def no_op():
     pass
+
+platform_detector = {
+    "Windows": load_windows,
+    "Darwin": load_mac,
+    "undetected": no_op
+}
+
+if __name__ == '__main__':
+    platform_detector.get(platform.system(), "undetected")()
